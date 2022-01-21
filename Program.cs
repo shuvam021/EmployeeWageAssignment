a5b02636ac0bc5c0c2102632e39cad1942af7324
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 20:08:43 +0530

    [Shuvam] Add employee attendance check added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@ namespace EmployeeWageAssignment
 {
     internal class Program
     {
+        const int IsFullTime = 1;
+        public static Random RandomValue = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Emp wage program");
+            int empCheck = RandomValue.Next(0, 2);
+            Console.WriteLine(empCheck == IsFullTime ? "Employee is present" : "Employee is Absent");
         }
     }
 }
