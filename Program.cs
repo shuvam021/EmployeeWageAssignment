e8d25d6f023527c945fcbc5a1d952a3f9d8b55af
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 20:04:37 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emp wage program");
+        }
+    }
+}
